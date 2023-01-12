13e572cd33fa4357b4d5a4c6fa14e29811df665a
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2023-01-12 23:39:05 +0300

    firstScene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
 import * as THREE from 'three';
-
+import FirstScene from './FirstScene';
 
 const width = window.innerWidth
 const height = window.innerHeight
 
 const renderer = new THREE.WebGLRenderer({
-	canvas: document.getElementById('app') as HTMLCanvasElement
+  canvas: document.getElementById('app') as HTMLCanvasElement
 })
 renderer.setSize(width, height)
 
 
-
+//field view  / aspect ratio / near plane /  far plane >>
 const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
-const scene = new THREE.Scene()
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshPhongMaterial({ color: 0xFFAD00 })
-const cube = new THREE.Mesh(geometry, material)
-cube.position.z = -5
-cube.position.y = 1
+const scene = new FirstScene()
+scene.initialize()
 
-scene.add(cube)
 
 renderer.render(scene, mainCamera)
